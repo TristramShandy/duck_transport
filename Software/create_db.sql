ca0f304e89281a12e90149bf0d582b016556f994
Michael Ulm <michael@mulm.at>
2015-06-30 19:33:07 +0200

    Added persons table

--- Software/create_db.sql
+++ Software/create_db.sql
@@ -2,3 +2,5 @@ create table books(id integer primary key, name text, volume integer);
 create table stories(id integer primary key, name text);
 create table stories_in_books(id integer primary key, story_id integer, book_id integer, page_start integer, page_end integer, foreign key(story_id) references stories(id), foreign key(book_id) references books(id));
 create table movements(id integer primary key, movers text, origin_place text, origin_time text, destination_place text, destination_time text, movement_mode text, purpose text, comment text, story_id integer, foreign key(story_id) references stories(id));
+create table persons(id integer primary key, name text);
+create table person_movement(movement_id integer, person_id integer, foreign key(movement_id) references movements(id), foreign key(person_id) references persons(id));
